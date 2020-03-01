376942802a36674c2b6e908998b65d715ebc3c0d
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-03-01 17:08:02 +0900

    add env

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,20 +6,24 @@
       </template>
     </TDNavigation>
     <v-content class="backGroundColor">
-      <div class="pa-4 white"></div>
+      <div class="pa-4 white">
+        <h1>{{ title }}</h1>
+        <h1>{{ uri }}</h1>
+      </div>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import TDNavigation from "@/components/TDNavigation";
+import TDNavigation from '@/components/TDNavigation';
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     TDNavigation
   },
   data: () => ({
-    title: "ホーム"
+    title: 'ホーム',
+    uri: process.env.VUE_APP_API_URL_BASE
   })
 };
 </script>
