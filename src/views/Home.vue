bddd34b841216371c6e76b3eec7bf8633513c809
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-03-01 17:45:43 +0900

    add Home axios&getTrello

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,6 @@
     <v-content class="backGroundColor">
       <div class="pa-4 white">
         <h1>{{ title }}</h1>
-        <h1>{{ uri }}</h1>
       </div>
     </v-content>
   </v-app>
@@ -16,6 +15,7 @@
 
 <script>
 import TDNavigation from '@/components/TDNavigation';
+import axios from 'axios';
 export default {
   name: 'Home',
   components: {
@@ -23,7 +23,33 @@ export default {
   },
   data: () => ({
     title: 'ホーム',
-    uri: process.env.VUE_APP_API_URL_BASE
-  })
+    item: null,
+    loading: false,
+    errored: false,
+    error: null
+  }),
+  async created() {
+    await this.getTrelloItem();
+  },
+  methods: {
+    getTrelloItem() {
+      axios.defaults.baseURL = process.env.VUE_APP_API_URL_BASE;
+      const url =
+        'boards/oIm50eVO/cards/?fields=name,url&' +
+        'key=' +
+        String(process.env.VUE_APP_API_KEY) +
+        '&token=' +
+        String(process.env.VUE_APP_API_TOKEN);
+      axios
+        .get(url)
+        .then(response => {
+          this.item = response.data;
+        })
+        .catch(err => {
+          (this.errored = true), (this.error = err);
+        })
+        .finally(() => (this.loading = false));
+    }
+  }
 };
 </script>
