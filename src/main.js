153d5415002e6b153a8c5c14f27a71dbdcff2ce9
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-03-01 13:59:52 +0900

    modify app&main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import 'babel-polyfill';
+import '@/styles/global.scss';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
