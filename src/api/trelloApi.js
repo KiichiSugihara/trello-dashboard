fc0df927dc28206eae29f2cc8049394e2b56def0
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-03-01 23:43:41 +0900

    add api trello

---
+++ src/api/trelloApi.js
@@ -0,0 +1,28 @@
+// trelloのAPI系Service
+import axios from 'axios';
+axios.defaults.baseURL = process.env.VUE_APP_API_URL_BASE;
+const keys =
+  '&key=' +
+  String(process.env.VUE_APP_API_KEY) +
+  '&token=' +
+  String(process.env.VUE_APP_API_TOKEN);
+const boardKey = process.env.VUE_APP_API_BOARD_KEY;
+
+class TrelloApi {
+  // Board自身の情報取得
+  async getBoard() {
+    const url = 'boards/' + boardKey + '?fields=name,url' + keys;
+    return axios.get(url);
+  }
+  async getLists() {
+    const url = 'boards/' + boardKey + '/lists/?fields=name,url' + keys;
+    return axios.get(url);
+  }
+  // // ListAPI
+  // getCards(listId) {
+  //   const url = '/lists/' + listId + '/cards?fields=name&' + keys;
+  //   return axios.get(url);
+  // }
+}
+
+export default new TrelloApi();
